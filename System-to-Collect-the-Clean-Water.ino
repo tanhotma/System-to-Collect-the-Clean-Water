c5245462ac9da422f66ea7b29b96567a550409fc
Marcus Mesta <Marcusmesta@protonmail.com>
2018-04-28 15:33:59 -0700

    Testing Valves
    wrote valve functions

---
+++ System-to-Collect-the-Clean-Water.ino
@@ -0,0 +1,41 @@
+//================  Arduino Pin Setup ================//
+
+const int cleanWaterValvePin = 0; //Pin controlling the clean water valve.
+const int dirtyWaterValvePin = 0; //Pin controlling the dirty water valve.
+
+bool cleanWaterValveState = false; //Normally closed. Make these 
+bool dirtyWaterValveState = false; //Normally closed. 
+
+void setup() {
+  
+//================= Iniatilize Arduino IO ===========//
+pinMode(cleanWaterValvePin, OUTPUT);
+pinMode(dirtyWaterValvePin, OUTPUT);
+
+
+}
+
+void loop() {
+
+  cleanValveState(true);
+  delay(500);
+  cleanValveState(false);
+  delay(500);
+  dirtyValveState(true);
+  delay(500);
+  dirtyValveState(false);
+  delay(500);
+
+}
+
+void cleanValveState(bool myState){ // 1 == OPEN || 0 == closed
+  cleanWaterValveState = myState;
+  digitalWrite(cleanWaterValvePin, cleanWaterValveState);
+}
+
+void dirtyValveState(bool myState){ // 1 == OPEN || 0 == closed
+  dirtyWaterValveState = myState;
+  digitalWrite(dirtyWaterValvePin, cleanWaterValveState);
+}
+
+
