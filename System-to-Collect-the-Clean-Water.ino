746c9f8264ba2cc93e7d7092576654b904b715c5
Marcus Mesta <Marcusmesta@protonmail.com>
2018-04-28 16:38:16 -0700

    Sonic Sensor update

--- System-to-Collect-the-Clean-Water.ino
+++ System-to-Collect-the-Clean-Water.ino
@@ -1,30 +1,69 @@
 //================  Arduino Pin Setup ================//
 
+
+//Valve Pins
 const int cleanWaterValvePin = 2; //Pin controlling the clean water valve.
 const int dirtyWaterValvePin = 3; //Pin controlling the dirty water valve.
 
 bool cleanWaterValveState = false; //Normally closed. Make these 
 bool dirtyWaterValveState = false; //Normally closed. 
 
+
+//Ultra-sonic sensor  pins + variables
+const int sonicTrigPin=9;
+const int sonicEchoPin=10;
+double mySonicDistance = 0.0;
+double myContainerRadius = 7;  // radius
+long sonicDuration;
+
 void setup() {
+Serial.begin(9600); // start serial debug
   
 //================= Iniatilize Arduino IO ===========//
+
+//Valve
 pinMode(cleanWaterValvePin, OUTPUT);
 pinMode(dirtyWaterValvePin, OUTPUT);
+
+//UltraSonic Sensor
+pinMode(sonicTrigPin, OUTPUT); //Sets the trigPin as an Output
+pinMode(sonicEchoPin, INPUT);  //Sets the echoPin as an Input
 
 
 }
 
 void loop() {
+sonicGetDistance();
+Serial.print("Volume: ");
+Serial.println(getWaterVolume()); // print current volume
+delay(500);
+}
 
-  cleanValveState(true);
-  delay(500);
-  cleanValveState(false);
-  delay(500);
-  dirtyValveState(true);
-  delay(500);
-  dirtyValveState(false);
-  delay(500);
+double getWaterVolume()
+{
+  // A = pi * r^2
+  //V = A * H
+  // H = sonic distance
+ double pi = 3.14;
+
+  return mySonicDistance * pi * myContainerRadius * myContainerRadius;
+}
+void sonicGetDistance(){
+
+//Clears the trigPin
+digitalWrite(sonicTrigPin, LOW);
+delayMicroseconds(2);
+//Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(sonicTrigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(sonicTrigPin, LOW);
+//Reads the echoPin, returns the sound wave travel time in microseconds
+sonicDuration=pulseIn(sonicTrigPin, HIGH);
+//Calculating the distance
+mySonicDistance=sonicDuration*0.034/2;
+//Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(mySonicDistance); //Reads distance in cm
 
 }
 
